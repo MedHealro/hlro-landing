af26c6ca9cca290e8614311fd1ae36c1738d1c5e
sudoris <zdent09@gmail.com>
2024-07-02 02:15:59 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="primary-header">
+    <div class="container">
+      <RouterLink to="/">
+        <img alt="logo" class="logo" src="@/assets/logo.svg" />
+      </RouterLink>
+      <nav class="primary-nav">
+        <ul role="list" class="nav-list">
+          <RouterLink to="/">解決方案</RouterLink>
+          <RouterLink to="/">關於我們</RouterLink>
+          <RouterLink to="/">聯絡我們</RouterLink>
+        </ul>
+      </nav>
+      <button class="button">取得演示</button>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.logo {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>Lorem Ipsum</template>
+
+<style scoped></style>
